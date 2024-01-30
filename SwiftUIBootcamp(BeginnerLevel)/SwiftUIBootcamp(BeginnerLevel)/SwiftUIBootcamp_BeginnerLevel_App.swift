c710024d9bfd18dfd8c70642b3679d91ad8e5230
kimscastle <uiskim22@pos.idserve.net>
2024-01-30 17:59:01 +0900

    [ADD] swiftUI bootcamp(beginner level)파일생성

---
+++ SwiftUIBootcamp(BeginnerLevel)/SwiftUIBootcamp(BeginnerLevel)/SwiftUIBootcamp_BeginnerLevel_App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIBootcamp_BeginnerLevel_App.swift
+//  SwiftUIBootcamp(BeginnerLevel)
+//
+//  Created by uiskim on 1/30/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIBootcamp_BeginnerLevel_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
