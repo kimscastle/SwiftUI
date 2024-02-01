89271b6f25c738f633c4111fe869432498a9f5a1
kimscastle <uiskim22@pos.idserve.net>
2024-02-01 11:05:30 +0900

    [Course] #3강

---
+++ SwiftUIBootcamp(BeginnerLevel)/SwiftUIBootcamp(BeginnerLevel)/ShapesBootcamp.swift
@@ -0,0 +1,19 @@
+//
+//  ShapesBootcamp.swift
+//  SwiftUIBootcamp(BeginnerLevel)
+//
+//  Created by uiskim on 2/1/24.
+//
+
+import SwiftUI
+
+struct ShapesBootcamp: View {
+    var body: some View {
+        RoundedRectangle(cornerRadius: 50)
+            .frame(width: 200, height: 200)
+    }
+}
+
+#Preview {
+    ShapesBootcamp()
+}
