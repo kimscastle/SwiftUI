5aae7677342c053e7d064a129d3e606a39b0df4f
kimscastle <uiskim22@pos.idserve.net>
2024-02-03 10:31:59 +0900

    [Course] #17강

---
+++ SwiftUIBootcamp(BeginnerLevel)/SwiftUIBootcamp(BeginnerLevel)/SafeAreaBootcamp.swift
@@ -0,0 +1,22 @@
+//
+//  SafeAreaBootcamp.swift
+//  SwiftUIBootcamp(BeginnerLevel)
+//
+//  Created by uiskim on 2/2/24.
+//
+
+import SwiftUI
+
+struct SafeAreaBootcamp: View {
+    var body: some View {
+        VStack {
+            Text("gggg")
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .top)
+        .background(.red)
+    }
+}
+
+#Preview {
+    SafeAreaBootcamp()
+}
