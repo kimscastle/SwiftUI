207c87f3d0aa4f9932a581eda7d3962abb3956c9
kimscastle <uiskim22@pos.idserve.net>
2024-02-14 13:38:17 +0900

    [ADD] 프로젝트세팅

---
+++ SwiftUI_Basic_Part1/SwiftUI_Basic_Part1/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUI_Basic_Part1
+//
+//  Created by uiskim on 2/14/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUI_Basic_Part1/SwiftUI_Basic_Part1/SwiftUI_Basic_Part1App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Basic_Part1App.swift
+//  SwiftUI_Basic_Part1
+//
+//  Created by uiskim on 2/14/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Basic_Part1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
