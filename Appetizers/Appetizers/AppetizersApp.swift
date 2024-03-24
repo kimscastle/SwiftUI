d439ba0c5d5266def2b11c1784d802792eacdbf1
kimscastle <uiskim22@pos.idserve.net>
2024-03-24 09:29:57 +0900

    [ADD] appetizer프로젝트생성

---
+++ Appetizers/Appetizers/AppetizersApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppetizersApp.swift
+//  Appetizers
+//
+//  Created by uiskim on 3/24/24.
+//
+
+import SwiftUI
+
+@main
+struct AppetizersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
