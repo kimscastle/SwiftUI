bbcd7b8ad1cd1437edd56561f9696dfed22d68e7
kimscastle <uiskim22@pos.idserve.net>
2024-03-24 09:23:20 +0900

    [ADD]coin앱 프로젝트생성 및 asset추가

---
+++ CoinSampleApp/CoinSampleApp/CoinSampleAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoinSampleAppApp.swift
+//  CoinSampleApp
+//
+//  Created by uiskim on 3/23/24.
+//
+
+import SwiftUI
+
+@main
+struct CoinSampleAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwiftUI_Basic_with_iOS17/SwiftUI_Basic_with_iOS17/iOS16/ChartBasic.swift
@@ -0,0 +1,51 @@
+//
+//  ChartBasic.swift
+//  SwiftUI_Basic_with_iOS17
+//
+//  Created by uiskim on 3/23/24.
+//
+
+import SwiftUI
+import Charts
+
+struct WeightModel: Identifiable {
+    var id = UUID().uuidString
+    var day: String
+    var weight: Double
+}
+
+var weightData: [WeightModel] = [
+    WeightModel(day: "Sun", weight: 77),
+    WeightModel(day: "Mon", weight: 80),
+    WeightModel(day: "Tue", weight: 90),
+    WeightModel(day: "Wed", weight: 82),
+    WeightModel(day: "Thu", weight: 70),
+    WeightModel(day: "Fri", weight: 60),
+    WeightModel(day: "Sat", weight: 88)
+]
+
+struct ChartBasic: View {
+    var body: some View                   
+        ScrollView {
+            VStack(spacing: 20) {
+                Text("1. 바형차트")
+                Chart(weightData) { item in
+                    BarMark(x: .value("Day", item.day),
+                            y: .value("Weight", item.weight))
+                }
+                .frame(height: 200)
+                
+                Text("2. 라인차트")
+                Chart(weightData) { item in
+                    LineMark(x: .value("Day", item.day), y: .value("Weight", item.weight))
+                }
+                .frame(height: 200)
+            }
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ChartBasic()
+}
