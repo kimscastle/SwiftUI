29990f1e3ba59c58f1d6c9c25f3fbb1f462216a3
kimscastle <uiskim22@pos.idserve.net>
2024-04-03 13:17:05 +0900

    [ADD] swiftfulThinking Spotify프로젝트생성

---
+++ SwiftfulThinking_Spotify/SwiftfulThinking_Spotify/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftfulThinking_Spotify
+//
+//  Created by uiskim on 4/3/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftfulThinking_Spotify/SwiftfulThinking_Spotify/SwiftfulThinking_SpotifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftfulThinking_SpotifyApp.swift
+//  SwiftfulThinking_Spotify
+//
+//  Created by uiskim on 4/3/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftfulThinking_SpotifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
