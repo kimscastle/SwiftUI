9c9978c885c63d45413029a4b4849a105ea96f43
kimscastle <uiskim22@pos.idserve.net>
2024-04-13 13:46:22 +0900

    [FEAT] Bumble 3강

--- SwiftfulThinking_Bumble/SwiftfulThinking_Bumble/Core/BumbleHomeView.swift
+++ SwiftfulThinking_Bumble/SwiftfulThinking_Bumble/Core/BumbleHomeView.swift
@@ -6,11 +6,17 @@
 //
 
 import SwiftUI
+import SwiftfulUI
 
 struct BumbleHomeView: View {
     
     var options = ["Everyone", "Trending"]
     @State var selection = "Everyone"
+    
+    @State private var allUsers: [User] = []
+    @State private var selectedIndex: Int = 0
+    @State private var cardOffset: [Int:Bool] = [:]
+    @State private var currenSwipeOffset: CGFloat = 0
     
     var body: some View {
         ZStack {
@@ -23,11 +29,115 @@ struct BumbleHomeView: View {
                 BumbleFilterView(options: options, selection: $selection)
                     .background(Divider(), alignment: .bottom)
                 
-                BumbleCardView()
-                Spacer()
+//                BumbleCardView()
+                
+                ZStack {
+                    if !allUsers.isEmpty {
+                        ForEach(Array(allUsers.enumerated()), id: \.offset) {
+                            index,
+                            user in
+                            let offsetValue = cardOffset[user.id]
+                            let isPrevious = (selectedIndex - 1) == index
+                            let isCurrent = selectedIndex == index
+                            let isNext = (selectedIndex + 1) == index
+                            if isPrevious || isCurrent || isNext {
+                                BumbleCardView(
+                                    user: user,
+                                    onSuperLikePressed: nil,
+                                    onXmarkPressed: {
+                                        userDidSelect(
+                                            index: index,
+                                            isLike: false
+                                        )
+                                    },
+                                    onCheckmarkPressed: {
+                                        userDidSelect(
+                                            index: index,
+                                            isLike: false
+                                        )
+                                    },
+                                    onSendComplimentPressed: nil,
+                                    onHideAndReportPressed: nil
+                                )
+                                    .withDragGesture(.horizontal, minimumDistance: 20, resets: true, rotationMultiplier: 1.05, onChanged: { dragOffset in
+                                        currenSwipeOffset = dragOffset.width
+                                    }, onEnded: { dragOffset in
+                                        if dragOffset.width < -100 {
+                                            userDidSelect(index: index, isLike: false)
+                                        } else if dragOffset.width > 100 {
+                                            userDidSelect(index: index, isLike: true)
+                                        }
+                                    })
+
+                                    .zIndex(Double(allUsers.count - index))
+                                    .offset(x: offsetValue == nil ? 0 : offsetValue == true ? 900 : -900)
+                            }
+                        }
+
+                    } else {
+                        ProgressView()
+                    }
+                    
+                    ZStack {
+                        Circle()
+                            .fill(.bumbleGray.opacity(0.4))
+                            .overlay {
+                                Image(systemName: "xmark")
+                                    .font(.title)
+                                    .fontWeight(.semibold)
+                            }
+                            .frame(width: 60, height: 60)
+                            .scaleEffect(abs(currenSwipeOffset) > 100 ? 1.5 : 1.0)
+                            .offset(x: min(-currenSwipeOffset, 150))
+                            .offset(x: -100)
+                            .frame(maxWidth: .infinity, alignment: .leading)
+                        
+                        Circle()
+                            .fill(.bumbleGray.opacity(0.4))
+                            .overlay {
+                                Image(systemName: "checkmark")
+                                    .font(.title)
+                                    .fontWeight(.semibold)
+                            }
+                            .frame(width: 60, height: 60)
+                            .scaleEffect(abs(currenSwipeOffset) > 100 ? 1.5 : 1.0)
+                            .offset(x: max(-currenSwipeOffset, -150))
+                            .offset(x: 100)
+                            .frame(maxWidth: .infinity, alignment: .trailing)
+
+                        
+                    }
+                    .animation(.smooth, value: currenSwipeOffset)
+                    .zIndex(9999)
+                } //:ZSTACK
+                .frame(maxHeight: .infinity)
+                .padding(4)
+                .animation(.smooth, value: cardOffset)
+                
+
             } //:VSTACK
             .padding(8)
         } //:ZSTACK
+        .task {
+            await getData()
+        }
+        .toolbar(.hidden, for: .navigationBar)
+    }
+    
+    private func getData() async {
+        guard allUsers.isEmpty else { return }
+        do {
+            allUsers = try await DatabaseHelper().getUsers()
+        } catch {
+            
+        }
+    }
+    
+    private func userDidSelect(index: Int, isLike: Bool) {
+        let user = allUsers[index]
+        cardOffset[user.id] = isLike
+        
+        selectedIndex += 1
     }
     
     private var header: some View {
--- SwiftfulThinking_Bumble/SwiftfulThinking_Bumble/SwiftfulThinking_BumbleApp.swift
+++ SwiftfulThinking_Bumble/SwiftfulThinking_Bumble/SwiftfulThinking_BumbleApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct SwiftfulThinking_BumbleApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            BumbleHomeView()
         }
     }
 }
